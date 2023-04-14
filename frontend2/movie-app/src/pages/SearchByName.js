2f8d02281401d7b2afe17991ca36c8a6601b874a
Kevin Infante <kinfante2019>
2023-04-14 14:21:32 -0400

    new frontend

---
+++ frontend2/movie-app/src/pages/SearchByName.js
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+//this page only generates a title, the search bar functionality is handled
+//by App.js
+export default function SearchByName() {
+    //console.log("Home has been called.");
+    return(
+        <h1 style={{margin:'15px'}}>Search Results</h1> //change later
+    )
+}
